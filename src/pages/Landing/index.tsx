0f39eae075f5d9b3dc8d7f7253dc889ecc53e66b
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-03 12:17:36 -0300

    logos exported

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
 
 function Landing() {
-    return <h1>Landing</h1>
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div id="logo-container">
+                    <img src={logoImg} alt="Proffy" />
+                    <h2>Your Online Study Platform</h2>
+                </div>
+                <img src="" alt="" className="hero-image" />
+            </div>
+        </div>
+    )
 }
-
 export default Landing;
