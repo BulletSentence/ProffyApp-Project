3ccc057596d17d0304953b3ab9f496d744413479
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-03 21:29:13 -0300

    Now using Link

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -21,15 +22,15 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="#" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Study
-                    </a>
+                    </Link>
 
-                    <a href="#" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar Aulas" />
                         Give Classes
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                     200 Connections Total! <img src={purpleHeartIcon} alt="Like" />
