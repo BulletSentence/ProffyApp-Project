affe506b925a3a72084c8e05b6d0f79f3605fd15
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-04 10:27:18 -0300

    styles for TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import backIcon from '../../assets/images/icons/back.svg';
+import logoImg from '../../assets/images/logo.svg';
+import './styles.css';
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to='/'>
+                        <img src={backIcon} alt="Back" />
+                    </Link>
+                    <img src={logoImg} alt="Logo" />
+                </div>
+
+                <div className="header-content">
+                    <strong>
+                        This are the Avaliable Proffys!
+                    </strong>
+                </div>
 
             </header>
         </div>
